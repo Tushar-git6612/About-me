5864bd2fe3b493b7e314b212c1b7960afdddbf2f
tushar <tush44644@gmail.com>
2023-11-15 19:30:10 +0530

    add code in image.tsx file

--- src/components/section1/Image.tsx
+++ src/components/section1/Image.tsx
@@ -1,13 +1,38 @@
-import{Fragment, memo} from 'react';
+import { Fragment, memo } from "react";
 // import reactCode from "../../Images/reactCode.jpg";
 import reactCode from "../../Images/reactCode.jpg";
+
+module.exports = {
+  // other webpack configurations...
+
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "images", // specify the output directory for images
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
 
 const Image = () => {
   return (
     <Fragment>
-      <img src={reactCode} alt="" className="w-100 h-100" style={{objectFit:"fill"}} />
+      <img
+        src={reactCode}
+        alt=""
+        className="w-100 h-100"
+        style={{ objectFit: "fill" }}
+      />
     </Fragment>
-  )
-}
+  );
+};
 
 export default memo(Image);
